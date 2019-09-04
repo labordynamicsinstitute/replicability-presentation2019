40093f806797039b10dac9874591e0928851f697
Lars Vilhuber <lars.vilhuber@cornell.edu>
2019-09-04 15:54:21 -0500

    Removing files not needed for release

--- programs/libraries.R
+++
@@ -1,9 +0,0 @@
-#' ---
-#' local libraries
-
-
-#' Define the list of libraries
-libraries <- c("dplyr","devtools","googlesheets","tidyr","ggplot2")
-
-results <- sapply(as.list(libraries), pkgTest)
-cbind(libraries,results)
